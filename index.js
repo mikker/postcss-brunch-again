f84bdc62cf2bdccfa0e948d3d1c61928b153da50
Mikkel Malmberg <mikkel@brnbw.com>
2016-08-25 10:40:28 +0200

    Rewrite, test import, fix basename

--- index.js
+++ index.js
@@ -1,82 +1,50 @@
-'use strict';
-const sysPath = require('path');
-const postcss = require('postcss');
-const progeny = require('progeny');
-const logger = require('loggy');
+const path = require('path')
+const progeny = require('progeny')
+const postcss = require('postcss')
 
-const defaultMapper = {
-	inline: false,
-	annotation: false,
-	sourcesContent: false,
-};
+class PostCSSPlugin {
+  constructor (config) {
+    const postcssConfig =
+      config && config.plugins && config.plugins.postcss || {}
 
-const pad = (stringeable, length) => {
-	return ' '.repeat(length - String(stringeable).length) + String(stringeable);
-};
+    this.mapOpts = Object.assign({
+      inline: false,
+      annotation: false,
+      sourcesContent: false
+    }, postcssConfig.map)
 
-const notify = (warnings) => {
-	if (!warnings.length) return;
-	const str = warnings.map(warn => {
-		const line = warn.line ? `line ${pad(warn.line, 4)}` : ''
-		const col = warn.col ? ` col ${pad(warn.col, 3)}` : ''
-		const node = warn.node ? ' ' + warn.node.toString() : '';
-		return `\t[${warn.plugin}]:${node}\t${line}${col}: ${warn.text}\n`;
-	}).join('\n');
-	logger.warn(`postcss-brunch: ${str}`);
+    this.getDependencies = progeny({
+      rootPath: config.paths.root,
+      reverseArgs: true
+    })
+
+    this.processor = postcss(postcssConfig.processors || [])
+  }
+
+  compile (file) {
+    const filePath = file.path
+    const map = Object.assign({}, this.mapOpts)
+    const opts = {
+      from: filePath,
+      to: filePath && path.basename(filePath),
+      map
+    }
+
+    return this.processor.process(file.data, opts)
+      .then((result) => {
+        return {
+          path: filePath,
+          data: result.css,
+          map: result.map.toJSON()
+        }
+      })
+  }
 }
 
-class PostCSSCompiler {
-	constructor(config) {
-		const rootPath = config.paths.root;
-		const cfg = config.plugins.postcss;
-		this.config = cfg || {};
-		const proc = cfg && cfg.processors || [];
-		this.map = this.config.map ?
-			Object.assign({}, defaultMapper, this.config.map) :
-			defaultMapper;
-		this.getDependencies = progeny({rootPath, reverseArgs: true});
-		this.processor = postcss(proc);
-	}
+Object.assign(PostCSSPlugin, {
+  brunchPlugin: true,
+  type: 'stylesheet',
+  extension: 'css'
+})
 
-	compile(file) {
-		const path = file.path;
-		const opts = {from: path, to: path, map: this.map};
-
-		if (file.map) {
-			opts.map.prev = file.map.toJSON();
-		}
-
-		return this.processor.process(file.data, opts).then(result => {
-			notify(result.warnings());
-
-			const mapping = result.map.toJSON();
-			// Not sure why postcss gives the basename instead of the full path;
-			// TODO: investigate.
-			// For now, "the solution":
-			const src = mapping.sources;
-			if (src && src.length === 1 && src[0] === sysPath.basename(path)) {
-				src[0] = path;
-			}
-
-			return {
-				path,
-				data: result.css,
-				map: mapping,
-			};
-		}).catch(error => {
-			if (error.name === 'CssSyntaxError') {
-				throw new Error('postcss-brunch syntax error: ' + error.message + error.showSourceCode());
-			}
-			throw error;
-		});
-	}
-}
-
-Object.assign(PostCSSCompiler.prototype, {
-	brunchPlugin: true,
-	type: 'stylesheet',
-	extension: 'css',
-});
-
-module.exports = PostCSSCompiler;
-
+module.exports = PostCSSPlugin
--- test.js
+++ test.js
@@ -1,84 +1,73 @@
-'use strict';
-const Plugin = require('./index');
-const fs = require('fs');
-const should = require('should');
-const Mocha = require('mocha');
+const PostCSSPlugin = require('./index')
+const fs = require('fs')
+const should = require('should')
+const Mocha = require('mocha')
 
-describe ('Tests', () => {
-   const blankOpts = { reporter: function(){} }; // no output
-   it('should fail', done => {
-      const failingTest = new Mocha.Test('failing Test', () => {
-         const data = fs.readFileSync('fixtures/sample.css', 'utf-8');
-         const expected = "this is actually not expected";
-         return plugin.process({data}).then(actual => {
-            actual.data.should.eql(expected);
-         });
-      });
-      const mocha = new Mocha(blankOpts);
-      mocha.suite.addTest(failingTest);
-      mocha.run(failures => {
-         failures.should.equal(1);
-         done();
-      })
-   });
-});
+const defaultProcessors = [
+  require('autoprefixer')({browsers: 'last 99 versions'}),
+  require('css-mqpacker'),
+  require('csswring')
+]
+const defaultConfig = {
+  paths: {root: '.'},
+  plugins: {
+    postcss: {
+      processors: defaultProcessors
+    }
+  }
+}
 
+describe('PostCSSPlugin', () => {
 
-describe('Plugin', () => {
-
-  let plugin, config;
+  let plugin, config
 
   beforeEach(() => {
-    config = {
-      paths: {root: '.'},
-      plugins: {
-        postcss: {
-          processors: [
-            require('autoprefixer')({browsers: 'last 99 versions'}),
-            require('css-mqpacker'),
-            require('csswring')
-          ]
-        }
-      }
-    };
-    plugin = new Plugin(config);
-  });
+    config = Object.assign({}, defaultConfig)
+  })
 
-  it('should be an object', () => {
-    plugin.should.be.an.Object;
-  });
+  it('is an object', () => {
+    new PostCSSPlugin(config).should.be.an.Object
+  })
 
-  // it('uses processors', () => {
-  //   plugin.processors.should.be.an.Array().with.length(3);
-  // });
+  it('compiles sample file', () => {
+     const data = fs.readFileSync('fixtures/sample.css', 'utf-8')
+     const expected = fs.readFileSync('fixtures/sample.out.css', 'utf-8')
 
-  it('compile', () => {
-    const data = 'a{a:a}';
-    return plugin.compile({data}).then(file => {
-      file.data.should.be.eql(data);
-    });
-  });
+    return new PostCSSPlugin(config).compile({data})
+      .then((actual) => {
+        actual.data.should.eql(expected)
+      })
+  })
 
-  it('compile with options', () => {
-     const data = fs.readFileSync('fixtures/sample.css', 'utf-8');
-     const expected = fs.readFileSync('fixtures/sample.out.css', 'utf-8');
-     return plugin.compile({data}).then(actual => {
-       actual.data.should.eql(expected);
-     });
-  });
-
-  it('compile with sourcemaps', () => {
-    const data = fs.readFileSync('fixtures/sample.css', 'utf-8');
+  it('compiles with sourcemaps', () => {
+    const data = fs.readFileSync('fixtures/sample.css', 'utf-8')
     const map = {
       version: 3,
-      sources: [ 'sample.css' ],
+      sources: [ 'fixtures/sample.css' ],
       names: [],
       mappings: 'AAKA,QACE,oBAAc,AAAd,qBAAc,AAAd,iBAAc,AAAd,oBAAc,AAAd,YAAc,CACf,AAPD,cACE,GACE,UAAY,CACb,AAMD,GACE,UAAY,CACb,CAPF',
       file: 'sample.css'
-    };
-    return plugin.compile({data, path: 'fixtures/sample.css'}).then(file => {
-      file.map.should.be.eql(map);
-    });
-  });
+    }
 
-});
+    return new PostCSSPlugin(config)
+      .compile({data, path: 'fixtures/sample.css'})
+      .then((file) => {
+        file.map.should.be.eql(map)
+      })
+  })
+
+  it('compiles with include', () => {
+    const data = fs.readFileSync('fixtures/bundle.css', 'utf-8')
+    const expected =
+      fs.readFileSync('fixtures/bundle.out.css', 'utf-8')
+
+    config.plugins.postcss.processors =
+      [require('postcss-import')()].concat(defaultProcessors)
+
+    return new PostCSSPlugin(config).compile({data})
+      .then((actual) => {
+        (actual.data + '\n').should.eql(expected)
+      })
+  })
+
+})
